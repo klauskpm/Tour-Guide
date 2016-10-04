46aff10e8389276844eb0a4677769ad3db5a43d3
Klaus Kazlauskas <klausmachado@gmail.com>
2016-10-04 11:32:48 -0300

    Simple list items

--- app/src/main/java/br/com/klauskpm/tourguide/EventsFragment.java
+++ app/src/main/java/br/com/klauskpm/tourguide/EventsFragment.java
@@ -1,6 +1,8 @@
 package br.com.klauskpm.tourguide;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 /**
@@ -15,4 +17,10 @@ public class EventsFragment extends CategoryFragment {
         setTitleResourceId(R.string.category_events);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        locationArrayList.add(new Location("Rua da Quitanda", -74.1231, 42.1231));
+    }
 }
