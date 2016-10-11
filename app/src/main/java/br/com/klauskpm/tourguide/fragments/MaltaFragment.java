f427cbecb4d30cd65ee8f002cfda7ef6e2251f11
Klaus Kazlauskas <klausmachado@gmail.com>
2016-10-11 10:57:54 -0300

    Malta Images

--- app/src/main/java/br/com/klauskpm/tourguide/fragments/MaltaFragment.java
+++ app/src/main/java/br/com/klauskpm/tourguide/fragments/MaltaFragment.java
@@ -23,9 +23,9 @@ public class MaltaFragment extends CategoryFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        locationArrayList.add(new Location("Kings Landing Gate", "Mdina Gate", 14.403344, 35.884589));
-        locationArrayList.add(new Location("Red Keep Gate", "Fort Ricasoli", 14.527209, 35.896228));
-        locationArrayList.add(new Location("Daenerys and Khal Drogo's Wedding", "Azure Window", 14.188042, 36.053517));
-        locationArrayList.add(new Location("Illyrio Mopatis's House", "Verdala Palace", 14.400482, 35.861507));
+        locationArrayList.add(new Location("Kings Landing Gate", "Mdina Gate", 14.403344, 35.884589, R.drawable.mdina_gate));
+        locationArrayList.add(new Location("Red Keep Gate", "Fort Ricasoli", 14.527209, 35.896228, R.drawable.fort_ricasoli));
+        locationArrayList.add(new Location("Daenerys and Khal Drogo's Wedding", "Azure Window", 14.188042, 36.053517, R.drawable.azure_window));
+        locationArrayList.add(new Location("Illyrio Mopatis's House", "Verdala Palace", 14.400482, 35.861507, R.drawable.verdala_palace));
     }
 }
