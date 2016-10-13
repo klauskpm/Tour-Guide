ad83455d79319826663a1806e966e2561420bdba
Klaus Kazlauskas <klausmachado@gmail.com>
2016-10-13 16:11:38 -0300

    String resources for CroatiaFragment

--- app/src/main/java/br/com/klauskpm/tourguide/fragments/CroatiaFragment.java
+++ app/src/main/java/br/com/klauskpm/tourguide/fragments/CroatiaFragment.java
@@ -24,9 +24,9 @@ public class CroatiaFragment extends CategoryFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        locationArrayList.add(new Location("King's Landing", "Dubrovnik", 18.093226, 42.650215, R.drawable.dubrovnik));
-        locationArrayList.add(new Location("Red Keep", "Lovrijenac Fortress", 18.104176, 42.640565, R.drawable.lovrijenac_fortress));
-        locationArrayList.add(new Location("House of the Undying", "Minčeta Tower", 18.108474, 42.642745, R.drawable.minceta_tower));
-        locationArrayList.add(new Location("Qarth", "Lokrum", 18.120097, 42.628536, R.drawable.lokrum));
+        locationArrayList.add(new Location(getString(R.string.kings_landing), getString(R.string.dubrovnik), 18.093226, 42.650215, R.drawable.dubrovnik));
+        locationArrayList.add(new Location(getString(R.string.red_keep), getString(R.string.lovrijenac_fortress), 18.104176, 42.640565, R.drawable.lovrijenac_fortress));
+        locationArrayList.add(new Location(getString(R.string.house_of_the_undying), getString(R.string.minceta_tower), 18.108474, 42.642745, R.drawable.minceta_tower));
+        locationArrayList.add(new Location(getString(R.string.qarth), getString(R.string.lokrum), 18.120097, 42.628536, R.drawable.lokrum));
     }
 }
