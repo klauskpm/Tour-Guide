24c58ac05644c9e7e9cfbfe5195ae48e77c13ab5
Klaus Kazlauskas <klausmachado@gmail.com>
2016-10-13 16:17:25 -0300

    String resources for IrelandFragment

--- app/src/main/java/br/com/klauskpm/tourguide/fragments/IrelandFragment.java
+++ app/src/main/java/br/com/klauskpm/tourguide/fragments/IrelandFragment.java
@@ -23,10 +23,10 @@ public class IrelandFragment extends CategoryFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        locationArrayList.add(new Location("Northern Forests", "Tollymore Forest Park", -5.942564, 54.219669, R.drawable.tollymore_forest_park));
-        locationArrayList.add(new Location("Winterfell", "Ward Castle", -5.585427, 54.369072, R.drawable.ward_castle));
-        locationArrayList.add(new Location("Dragonstone Castle beach", "Mussenden Temple", -6.810731, 55.167690, R.drawable.mussenden_temple));
-        locationArrayList.add(new Location("King's Landing road", "The Dark Hedges", -6.380715, 55.134564, R.drawable.the_dark_hedges));
-        locationArrayList.add(new Location("Lordsport", "Ballintoy Harbour", -6.368850, 55.244179, R.drawable.ballintoy_harbour));
+        locationArrayList.add(new Location(getString(R.string.northern_forests), getString(R.string.tollymore_forest_park), -5.942564, 54.219669, R.drawable.tollymore_forest_park));
+        locationArrayList.add(new Location(getString(R.string.winterfell), getString(R.string.ward_castle), -5.585427, 54.369072, R.drawable.ward_castle));
+        locationArrayList.add(new Location(getString(R.string.dragonstone_castle_beach), getString(R.string.mussenden_temple), -6.810731, 55.167690, R.drawable.mussenden_temple));
+        locationArrayList.add(new Location(getString(R.string.king_landing_road), getString(R.string.the_dark_hedges), -6.380715, 55.134564, R.drawable.the_dark_hedges));
+        locationArrayList.add(new Location(getString(R.string.lordsport), getString(R.string.ballintoy_harbour), -6.368850, 55.244179, R.drawable.ballintoy_harbour));
     }
 }
